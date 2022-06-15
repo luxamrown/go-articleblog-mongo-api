c41f63e651b76a54b552757751702d9288fe4955
luxamrown <luxamrownn@gmail.com>
2022-06-15 08:15:29 +0700

    anjing banget heroku

--- config/config.go
+++ config/config.go
@@ -27,9 +27,18 @@ type Config struct {
 	DbConfig
 }
 
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
+
 func (c Config) SetConfig() Config {
-	urlApi := os.Getenv("PORT")
-	c.ApiConfig = ApiConfig{Url: ":" + urlApi}
+	c.ApiConfig = ApiConfig{Url: GetPort()}
 	c.DbConfig = DbConfig{
 		Uri: "mongodb+srv://luxamrown:%40Bulungan2018@blog.yzqxr.mongodb.net/?retryWrites=true&w=majority",
 	}
